dd9f691e2adeece00f7613e2c22301274713ac21
hwncito <juanchosperoni@hotmail.com>
2024-11-24 21:21:02 -0300

    added scroll animations to image and text in living section

--- src/components/living/Living.tsx
+++ src/components/living/Living.tsx
@@ -1,22 +1,105 @@
+'use client';
 import Image from 'next/image';
 import Button from '../ui/Button';
+import { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import SplitType from 'split-type';
 
 export default function Living() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Animate the image cover
+    const animateCover = () => {
+      gsap.to('#image-cover', {
+        height: 0,
+        duration: 2,
+        ease: 'power3.out',
+      });
+    };
+
+    ScrollTrigger.create({
+      trigger: '#image-cover',
+      start: 'top 90%',
+      onEnter: animateCover,
+      toggleActions: 'play none none none',
+      once: true,
+    });
+
+    // Animate the text
+    const animateText = () => {
+      const text = new SplitType('.animated-text', { types: 'chars' });
+      const chars = text.chars;
+
+      if (chars) {
+        const tl = gsap.timeline();
+        tl.fromTo(
+          chars,
+          {
+            scaleY: 0,
+            y: -20,
+          },
+          {
+            opacity: 1,
+            scaleY: 1,
+            y: 0,
+            transformOrigin: 'top',
+            duration: 1,
+            stagger: 0.05,
+          }
+        );
+        tl.set('.animated-text', { visibility: 'visible' }, '<');
+        tl.to('#cross-text', {
+          width: '360px',
+          opacity: 1,
+          duration: 2,
+          ease: 'power3.out',
+        })
+      }
+    };
+
+    ScrollTrigger.create({
+      trigger: '#living-text',
+      start: 'top 80%',
+      onEnter: animateText,
+      toggleActions: 'play none none none',
+      once: true,
+    });
+  }, []);
+
   return (
     <section className="py-20 h-screen flex justify-between items-center gap-8 mx-8">
-      <Image
-        className="rounded-3xl"
-        src={'/boat.webp'}
-        alt="Boat in the middle of the sea"
-        width={600}
-        height={0}
-      />
-      <div className="w-full pl-8 flex flex-col">
+      <div className="relative w-full max-w-[600px]">
+        <div
+          id="image-cover"
+          className="absolute top-0 bg-white w-full h-full"
+        ></div>
+        <Image
+          className="rounded-3xl"
+          src={'/boat.webp'}
+          alt="Boat in the middle of the sea"
+          width={600}
+          height={0}
+        />
+      </div>
+
+      <div id="living-text" className="w-full pl-8 flex flex-col">
         <ul className="relative text-6xl space-y-6">
           <li>Use your time</li>
-          <li className="text-[#1D545E]">living your vacation,</li>
-          <div className="absolute bottom-5 w-[360px] border-2 border-gray-500"></div>
-          <li>not booking it.</li>
+          <li
+            className="animated-text text-[#1D545E]"
+            style={{ visibility: 'hidden' }}
+          >
+            living your vacation,
+          </li>
+          <li className="animated-text" style={{ visibility: 'hidden' }}>
+            not booking it.
+          </li>
+          <div
+            id="cross-text"
+            className="absolute bottom-5 w-[0px] border-2 border-gray-500 opacity-0"
+          ></div>
         </ul>
         <Button
           id="living-button"
